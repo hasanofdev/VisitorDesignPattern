8c853792eb0a3556df9019566dbf889d68155b8a
Hese_fz25 <Hese_fz25@STHQ0126-15>
2022-11-30 13:16:25 +0400

    Add project files.

---
+++ VisitorDesignPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿interface IVisitor
+{
+    public void visitElementA(ElementA element);
+    public void visitElementB(ElementB element);
+}
+
+interface IElement
+{
+    public void Accept(IVisitor visitor);
+}
+
+class ElementA : IElement
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.visitElementA(this);
+    }
+
+    public string FeatureA() =>"Element A";
+}
+
+class ElementB : IElement
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.visitElementB(this);
+    }
+
+    public string FeatureB() =>"Element B";
+}
+
+class XMLConverter : IVisitor
+{
+    public void visitElementA(ElementA element)
+    {
+        Console.WriteLine(element.FeatureA() + " is converted to XML format!");
+    }
+
+    public void visitElementB(ElementB element)
+    {
+        Console.WriteLine(element.FeatureB() + " is converted to XML format!");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IVisitor visitor = new XMLConverter();
+
+        List<IElement> elements = new List<IElement>()
+        {
+            new ElementA(),
+            new ElementB(),
+        };
+
+        foreach (var element in elements)
+        {
+            element.Accept(visitor);
+        }
+    }
+}
+
